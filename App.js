cbabccc25baa9f40c9e2de5763a66280db18eb66
CristoferDuarte <87478927+CristoferDuarte@users.noreply.github.com>
2022-05-23 17:00:05 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,10 @@
+import React, {Component} from "react";
+import BottomTabNavigator from "./Componentes/BottomTabNavigator";
+
+export default class App extends Component {
+  render(){
+    return <BottomTabNavigator/>
+  }
+    
+  
+}
